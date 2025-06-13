4489c8a7146cb20b51b5a6d73afa0e719767ede8
Kayden Ireland <kaydenireland@aol.com>
2025-06-13 19:13:49 -0400

    Default Div Setup, Simple Style

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
 
+
+
+// Temp Demo
+ctx.fillStyle = '#309030';
+ctx.font = "30px Arial";
+ctx.fillText("Solitaire Canvas Loaded", canvas.width/3, canvas.height/2);
